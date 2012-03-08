e130fc44370b1ba4dd00811de93d0fc485670ee0
Novikov Sergey <svnovikov@gmail.com>
2012-03-08 14:47:09 +0100

    Exircise #11

---
+++ operators/Exircise011.java
@@ -0,0 +1,24 @@
+//: operators/Exircise011.java
+import static net.mindview.util.Print.*;
+
+public class Exircise011 {
+  public static void main(String[] args) {
+    short s = 0;
+    int i = 0x8000000;
+    print("    i  : " + Integer.toBinaryString(i));
+    for (s=1; s<8; s++)
+    {
+		i >>>= 4;
+		print("i>>>" + s*4 + (s*4<10?" ":"") +" : " + Integer.toBinaryString(i));
+	}
+  }
+} /* Output:
+    i  : 1000000000000000000000000000
+i>>>4  : 100000000000000000000000
+i>>>8  : 10000000000000000000
+i>>>12 : 1000000000000000
+i>>>16 : 100000000000
+i>>>20 : 10000000
+i>>>24 : 1000
+i>>>28 : 0
+*///:~
