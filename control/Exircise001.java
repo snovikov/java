7908f5c723d1b7f1adb6cdf969fd8fa78865a03b
Novikov Sergey <svnovikov@gmail.com>
2012-06-10 19:37:51 +0200

    Exircise #1

---
+++ control/Exircise001.java
@@ -0,0 +1,22 @@
+//: control/ListCharacters.java
+// Demonstrates "for" loop by listing
+// all the lowercase ASCII letters.
+
+public class Exircise001 {
+  public static void main(String[] args) {
+    for(int c = 1; c <= 100; c++)
+        System.out.println("value: " + c);
+  }
+} /* Output:
+value: 1
+value: 2
+value: 3
+value: 4
+value: 5
+value: 6
+value: 7
+value: 8
+value: 9
+value: 10
+...
+*///:~
